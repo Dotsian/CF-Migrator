62f480fa13809dc9690795706051c8e2586babb4
Dotsian <selbydarryl05@gmail.com>
2025-09-26 00:37:38 -0400

    Improve UI

--- src/export.py
+++ src/export.py
@@ -3,14 +3,16 @@ import os
 import time
 from typing import Any
 
+import discord
+
 from carfigures.core.models import Player, CarType, Country, Car, CarInstance, GuildConfig
 
-__version__ = "1.0.0"
+__version__ = "1.0"
 
 MIGRATIONS: dict[str, dict[str, Any]] = {
     "R": {
         "model": CarType,
-        "process": "cartypes",
+        "process": "CarType",
         "values": [
             "name",
             "image",
@@ -18,7 +20,7 @@ MIGRATIONS: dict[str, dict[str, Any]] = {
     },
     "E": {
         "model": Country,
-        "process": "countries",
+        "process": "Country",
         "values": [
             "name",
             "image",
@@ -26,7 +28,7 @@ MIGRATIONS: dict[str, dict[str, Any]] = {
     },
     "B": {
         "model": Car,
-        "process": "cars",
+        "process": "Car",
         "values": [
             "cartype_id",
             "fullName",
@@ -51,7 +53,7 @@ MIGRATIONS: dict[str, dict[str, Any]] = {
     },
     "BI": {
         "model": CarInstance,
-        "process": "car instances",
+        "process": "CarInstance",
         "values": [
             "car_id",
             "player_id",
@@ -70,17 +72,50 @@ MIGRATIONS: dict[str, dict[str, Any]] = {
     },
     "P": {
         "model": Player,
-        "process": "players",
+        "process": "Player",
         "values": ["discord_id"],
         "defaults": {"donationPolicy": 1, "privacyPolicy": 1},
     },
     "GC": {
         "model": GuildConfig,
-        "process": "guild configs",
+        "process": "GuildConfig",
         "values": ["guild_id"],
         "defaults": {"spawnChannel": None, "enabled": True},
     },
 }
+
+
+output = []
+
+
+def reload_embed(start_time: float | None = None, file: str | None = None, status="RUNNING"):
+    embed = discord.Embed(
+        title="CF-Migrator Process",
+        description=f"Status: **{status}**",
+    )
+
+    match status:
+        case "RUNNING":
+            embed.color = discord.Color.yellow()
+        case "FINISHED":
+            embed.color = discord.Color.green()
+        case "CANCELED":
+            embed.color = discord.Color.red()
+
+    if len(output) > 0:
+        embed.add_field(name="Output", value="\n".join(output))
+
+    if file:
+        embed.add_field(
+            name="File",
+            value=f"Saved to `/{file}` ({convert_size(os.path.getsize(file))})",
+            inline=False,
+        )
+
+    if start_time is not None:
+        embed.set_footer(text=f"Finished migration in {round((time.time() - start_time), 3)}s")
+
+    return embed
 
 
 def convert_size(bytes: int) -> str:
@@ -123,20 +158,23 @@ async def process(entry: str, migration) -> str:
 
         content.append("╵".join(fields))
 
-    await ctx.send(f"Migrated **{await  migration["model"].all().count():,}** {migration["process"]}")  # type: ignore  # noqa: F821
+    output.append(
+        f"- Migrated **{await migration["model"].all().count():,}** {migration["process"]} objects."
+    )
 
     return "\n".join(content)
 
 
-async def migrate(filename: str):
+async def migrate(message, filename: str):
     with bz2.open(f"{filename}.bz2", "wt", encoding="utf-8") as f:
         content = [
-            f"- Generated with 'CF-Migrator' v{__version__}\n"
-            "- Please do not modify this file unless you know what you're doing.\n\n"
+            f"// Generated with 'CF-Migrator' v{__version__}\n"
+            "// Please do not modify this file unless you know what you're doing.\n\n"
         ]
 
         for key, migration in MIGRATIONS.items():
             content.append(await process(key, migration))
+            await message.edit(embed=reload_embed())
 
         f.write("\n".join(content))
 
@@ -148,17 +186,13 @@ async def main():
         print("You cannot run this command from Ballsdex.")
         return
 
-    await ctx.send("Migrating...")  # type: ignore # noqa: F821
+    message = await ctx.send(embed=reload_embed())  # type: ignore # noqa: F821
 
-    t1 = time.time()
+    start_time = time.time()
 
-    path = await migrate("Migration.txt")
+    path = await migrate(message, "migration.txt")
 
-    t2 = time.time()
-
-    await ctx.send(  # type: ignore # noqa: F821
-        f"Finished migrating in `{round((t2 - t1), 3)}s`!\nFile was saved to `/{path}` ({convert_size(os.path.getsize(path))})"
-    )
+    await message.edit(embed=reload_embed(start_time, path, "FINISHED"))
 
 
 await main()  # type: ignore  # noqa: F704
